026ff5a8ec0172b2bc2fed30f59b35d8745c49ee
Mohamad Rahimi <m.rahimi.vasteras@gmail.com>
2024-08-20 10:33:02 +0200

    car

---
+++ main.py
@@ -0,0 +1,48 @@
+from car import Car
+from timeit import default_timer
+import string 
+
+ALPHABET = list(string.ascii_uppercase)
+#stora bokstäver === ändra inte sen låt det vara för fan
+DIGITS = list(string.digits)
+
+def create_all_cars_with_all_licence_plates() -> list:
+    l = []
+    for first_pos in ALPHABET:
+        for second_pos in ALPHABET:
+            for third_pos in ALPHABET:
+                for i in DIGITS:
+                    for j in DIGITS:
+                        for k in DIGITS:
+                            regnr = first_pos + second_pos + third_pos + i + j +k
+                            l.append(Car(regnr, '', ''))
+    return l
+
+def get_car_by_regno(regnr:str, list_of_cars:list[Car]):
+    for car in list_of_cars:
+        if car.regnr == regnr:
+            return car
+        
+    return None
+
+
+def main():
+    t_start = default_timer()
+    cars = create_all_cars_with_all_licence_plates()
+    t_end = default_timer()
+
+    print(f"Det tog {t_end - t_start} sekunder att spara alla bilar")
+    print(cars[0], cars[1000], cars[700000], cars[-1])
+
+    while True:
+        regnr = input ("Ange Regnr: ")
+        if regnr == "q": break
+
+        car = get_car_by_regno(regnr, cars)
+        if car :
+            print("Hitta bil!")
+        else:
+            print("Bil finns inte")
+
+if __name__ == '__main__':
+    main()
