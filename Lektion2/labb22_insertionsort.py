0f2b1aae0de32f68e60afe7cfeede4ce7b55c3ea
Mohamad Rahimi <m.rahimi.vasteras@gmail.com>
2024-08-22 12:31:53 +0200

    insert

--- Lektion2/labb22_insertionsort.py
+++ Lektion2/labb22_insertionsort.py
@@ -1,17 +1,20 @@
 spelare = ["Ronaldo", "Messi", "De Bruyne", "Zlatan", "Kaka", "Zidane", "Neymar", "Buffon", "Bale", "Totti"]
 
 
+
 def insertion_sort(lista):
     n = len(lista)
-    for i in range(n):
-        minsta_index = i #vi TROR att den är den minsta
-        for j in range(i+1, n): # sen jämför vi med resten
-            if lista[j] < lista[i]:
-                minsta_index = j #om nästa var större och vi hade trott fel då byter vi plats
-        lista[i],lista[minsta_index] = lista[minsta_index],lista[i]
+    for i in range(1, n):
+        key = lista[i]
+        j = i -1
+        while j >= 0 and key < lista[j]:
+            lista[j + 1] = lista[j]
+            j = j -1
+        lista[j+1] = key
 
     return lista
 
-insertion_sort_sorted = insertion_sort(spelare)
-print(insertion_sort_sorted)
 
+insert1 = insertion_sort(spelare)
+
+print(insert1)
