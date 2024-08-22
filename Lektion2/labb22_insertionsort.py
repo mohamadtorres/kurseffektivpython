4d56d20addbdaa22a196347e00218f2af657d0b5
Mohamad Rahimi <m.rahimi.vasteras@gmail.com>
2024-08-22 14:20:49 +0200

    1

--- Lektion2/labb22_insertionsort.py
+++ Lektion2/labb22_insertionsort.py
@@ -2,16 +2,23 @@ spelare = ["Ronaldo", "Messi", "De Bruyne", "Zlatan", "Kaka", "Zidane", "Neymar"
 
 
 
-def insertion_sort(lista):
-    n = len(lista)
-    for i in range(1, n):
-        key = lista[i]
-        j = i -1
-        while j >= 0 and key < lista[j]:
-            lista[j + 1] = lista[j]
-            j = j -1
-        lista[j+1] = key
+# def insertion_sort(lista):
+#     n = len(lista)
+#     for i in range(1, n):
+#         key = lista[i]
+#         j = i -1
+#         while j >= 0 and key < lista[j]:
+#             lista[j + 1] = lista[j]
+#             j = j -1
+#         lista[j+1] = key
 
+#     return lista
+def insertion_sort(lista):
+    for i in range(1,len(lista)):
+        j = i
+        while lista [j -1] > lista [j] and j>0:
+            lista[j-1], lista[j] = lista[j], lista[j-1]
+            j -= 1
     return lista
 
 
